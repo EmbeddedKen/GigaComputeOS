fd95199d48894d6d7f4961f6ea00c26b390d470f
KennethVorseth <kennethvorseth@outlook.com>
2018-06-16 04:54:39 -0700

    Updated Build/Run Batch Script

--- osdev-kernel.bat
+++ osdev-kernel.bat
@@ -12,11 +12,11 @@ if /i "%val%"=="B" (
 echo [Build Only Selected]
 echo -Starting Build...
 echo -Assembling Bootloader...
-i686-elf-as src\boot.s -o obj/boot.o
+tools\i686-elf\bin\i686-elf-as src\boot.s -o obj/boot.o
 echo -Compiling Freestanding C Kernel...
-i686-elf-gcc -c src\kernel.c -o obj\kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wformat=0
+tools\i686-elf\bin\i686-elf-gcc -c src\kernel.c -o obj\kernel.o -std=gnu99 -ffreestanding -O2 -Wall -Wextra -Wformat=0
 echo -Linking...
-i686-elf-gcc -T src\linker.ld -o iso\os.bin -ffreestanding -O2 -nostdlib obj\boot.o obj\kernel.o -lgcc
+tools\i686-elf\bin\i686-elf-gcc -T src\linker.ld -o iso\os.bin -ffreestanding -O2 -nostdlib obj\boot.o obj\kernel.o -lgcc
 echo -Build Complete.
 )
 
@@ -24,11 +24,11 @@ if /i "%val%"=="R" (
 echo [Build and Run Selected]
 echo -Starting Build...
 echo -Assembling Bootloader...
-i686-elf-as src\boot.s -o obj/boot.o
+tools\i686-elf\bin\i686-elf-as src\boot.s -o obj/boot.o
 echo -Compiling Freestanding C Kernel Set1
-i686-elf-gcc -c src\kernel.c -o obj\kernel.o -std=gnu99 -ffreestanding -O2 -Wformat=0
+tools\i686-elf\bin\i686-elf-gcc -c src\kernel.c -o obj\kernel.o -std=gnu99 -ffreestanding -O2 -Wformat=0
 echo -Linking...
-i686-elf-gcc -T src\linker.ld -o iso\os.bin -ffreestanding -O2 -nostdlib obj\boot.o obj\kernel.o -lgcc
+tools\i686-elf\bin\i686-elf-gcc -T src\linker.ld -o iso\os.bin -ffreestanding -O2 -nostdlib obj\boot.o obj\kernel.o -lgcc
 echo -Build Complete.
 echo -Running OS Kernel via QEMU...
 qemu-system-i386 -kernel iso/os.bin -k en-us
